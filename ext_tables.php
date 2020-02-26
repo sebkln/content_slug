c2bc45d17f3970537c61794f0c469ec2772d5101
Sebastian Klein <sebastian@sklein-medien.de>
2020-02-26 20:39:36 +0100

    [TASK] Improve label, add Context Sensitive Help

---
+++ ext_tables.php
@@ -0,0 +1,7 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tt_content',
+    'EXT:content_slug/Resources/Private/Language/locallang_csh_tt_content.xlf'
+);
