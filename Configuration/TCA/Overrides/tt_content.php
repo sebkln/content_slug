18e2218d895611632dcc47c5dc594e0495386420
Sebastian Klein <sebastian@sklein-medien.de>
2020-02-27 21:11:42 +0100

    [FEATURE] Allow editors to add anchor link to header
    This adds a new checkbox in the content element.
    An editor can use this checkbox to activate an anchor link,
    which is then rendered next to the header.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,16 +13,26 @@ $fields = array(
             'eval' => 'trim,Sebkln\\ContentSlug\\Evaluation\\FragmentEvaluation,uniqueInPid',
             'default' => ''
         ],
+    ],
+    'tx_content_slug_link' => [
+        'exclude' => 1,
+        'label' => 'LLL:EXT:content_slug/Resources/Private/Language/locallang_db.xlf:tt_content.tx_content_slug_link',
+        'config' => [
+            'type' => 'check',
+            'items' => [
+                ['LLL:EXT:content_slug/Resources/Private/Language/locallang_db.xlf:tt_content.tx_content_slug_link.check', ''],
+            ],
+        ],
     ]
 );
 
-// Add the new field to an existing table definition:
+// Add the new fields to an existing table definition:
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $fields);
 
-// Add the new field to an existing palette:
+// Add the new fields to an existing palette:
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
     'tt_content', // Table for TYPO3 content elements
     'headers', // Existing palette for header related fields
-    '--linebreak--, tx_content_slug_fragment', // The new field, rendered in a new line
+    '--linebreak--, tx_content_slug_fragment, tx_content_slug_link', // The new fields, rendered in a new line
     'after:header_link' // Position of the new field
 );
--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,6 @@
 # Modifying tt_content table
 #
 CREATE TABLE tt_content (
-    tx_content_slug_fragment varchar(255) DEFAULT '' NOT NULL
+    tx_content_slug_fragment varchar(255) DEFAULT '' NOT NULL,
+    tx_content_slug_link TINYINT(1) UNSIGNED DEFAULT '0' NOT NULL
 );
