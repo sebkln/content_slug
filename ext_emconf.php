0aa31e8606ea2304e2b644073ddd3529d0f7d903
Sebastian Klein <sebastian@sklein-medien.de>
2021-02-01 20:45:39 +0100

    [TASK] Update TYPO3 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.4.99',
+            'typo3' => '8.7.0-11.1.99',
             'fluid_styled_content' => ''
         ],
         'conflicts' => [
