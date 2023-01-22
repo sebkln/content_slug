3addb4e6ed9a612ffab6ca8d4eaf7442a05ebb36
Sebastian Klein <laitnin@gmx.net>
2023-01-22 17:41:12 +0100

    [TASK] Use constant 'TYPO3' in global script files

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,6 @@
 <?php
-defined('TYPO3_MODE') or die();
+
+defined('TYPO3') or die();
 
 // Register the class to be available in 'eval' of TCA:
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals']['Sebkln\\ContentSlug\\Evaluation\\FragmentEvaluation'] = '';
