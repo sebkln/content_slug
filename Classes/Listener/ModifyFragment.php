e66e90829b0dfe83fec8bd41cf2413b658101c7c
Sebastian Klein <laitnin@gmx.net>
2023-01-27 20:14:43 +0100

    [TASK] Use PSR-14 event to modify fragment in RTE and TCA links
    The event listener replaces the former hook
    'typolinkModifyParameterForPageLinks' for TYPO3 v12 and later.
    The hook is still provided for TYPO3 v11, until support is dropped.

--- Classes/Listener/ModifyFragment.php
+++ Classes/Listener/ModifyFragment.php
@@ -1,32 +1,30 @@
 <?php
 
-namespace Sebkln\ContentSlug\Hooks;
+namespace Sebkln\ContentSlug\Listener;
 
 /*
  * This file is part of the package sebkln/content_slug
  *
- * Copyright (c) 2021 Sebastian Klein <sebastian@sebkln.de>
+ * Copyright (c) 2023 Sebastian Klein <sebastian@sebkln.de>
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
 
-use Doctrine\DBAL\Exception as DBALException;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use Doctrine\DBAL\Exception;
 use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
-use TYPO3\CMS\Frontend\ContentObject\TypolinkModifyLinkConfigForPageLinksHookInterface;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\Event\ModifyPageLinkConfigurationEvent;
 
 /**
  * Replaces the default fragment (like "#c123") with the human-readable version, if given.
  * This applies to links in the RTE, as well as TCA fields with renderType "inputLink".
  *
- * TODO: Remove hook when support for TYPO3 v11 is dropped.
- *
- * @package Sebkln\ContentSlug\Hooks
+ * @package Sebkln\ContentSlug\Listener
  */
-class ReplaceFragment implements TypolinkModifyLinkConfigForPageLinksHookInterface
+class ModifyFragment
 {
     protected ConfigurationManagerInterface $configurationManager;
 
@@ -37,25 +35,24 @@ class ReplaceFragment implements TypolinkModifyLinkConfigForPageLinksHookInterfa
     }
 
     /**
-     * @param array $linkConfiguration The link configuration (for options see TSRef -> typolink)
-     * @param array $linkDetails Additional information for the link
-     * @param array $pageRow The complete page row for the page to link to
-     * @return array The modified $linkConfiguration
-     * @throws DBALException
+     * @throws Exception
      */
-    public function modifyPageLinkConfiguration(array $linkConfiguration, array $linkDetails, array $pageRow): array
+    public function __invoke(ModifyPageLinkConfigurationEvent $event): void
     {
-        if (isset($linkDetails['fragment']) && is_numeric($linkDetails['fragment'])) {
+        $fragment = $event->getFragment();
+        $fragment = substr($fragment, 1);
+
+        if (!empty($fragment) && is_numeric($fragment)) {
             // 1. Get TypoScript configuration:
             $settings = $this->configurationManager->getConfiguration(
                 ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT
             );
             $replaceFragmentInPageLinks = $settings['plugin.']['tx_contentslug.']['settings.']['replaceFragmentInPageLinks'];
 
-            // 2. Check if the hook should be used:
+            // 2. Check if fragment should be replaced:
             if ((int)$replaceFragmentInPageLinks === 1) {
                 // 3. Get data array of the linked content element:
-                $contentId = $linkDetails['fragment'];
+                $contentId = $fragment;
                 $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
                 $queryResult = $queryBuilder
                     ->select('*')
@@ -79,11 +76,9 @@ class ReplaceFragment implements TypolinkModifyLinkConfigForPageLinksHookInterfa
                     $recordContentObjectRenderer->start($queryResult, 'tt_content');
                     $recordContentObjectRenderer->setCurrentVal((string)$contentId);
                     $newFragment = $recordContentObjectRenderer->cObjGetSingle($fragmentcObj, $fragmentConf, 'newFragment');
-
-                    $linkConfiguration['section.']['override'] = $newFragment;
+                    $event->setFragment($newFragment);
                 }
             }
         }
-        return $linkConfiguration;
     }
 }
--- ext_localconf.php
+++ ext_localconf.php
@@ -3,18 +3,24 @@
 use Sebkln\ContentSlug\Evaluation\FragmentEvaluation;
 use Sebkln\ContentSlug\FormEngine\FieldControl\GenerateFragmentFromHeaderControl;
 use Sebkln\ContentSlug\Hooks\ReplaceFragment;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 defined('TYPO3') or die();
 
-// Register the class to be available in 'eval' of TCA:
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][FragmentEvaluation::class] = '';
+(static function () {
+    // Register the class to be available in 'eval' of TCA:
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tce']['formevals'][FragmentEvaluation::class] = '';
 
-// Register new render type to copy current header to the fragment field:
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1587208269] = [
-    'nodeName' => 'generateFragmentFromHeaderControl',
-    'priority' => 30,
-    'class' => GenerateFragmentFromHeaderControl::class
-];
+    // Register new render type to copy current header to the fragment field:
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1587208269] = [
+        'nodeName' => 'generateFragmentFromHeaderControl',
+        'priority' => 30,
+        'class' => GenerateFragmentFromHeaderControl::class
+    ];
 
-// Register hook to overwrite fragments in page links (RTE and TCA):
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typolinkProcessing']['typolinkModifyParameterForPageLinks'][] = ReplaceFragment::class;
+    // TODO: Remove hook when support for TYPO3 v11 is dropped.
+    if ((new Typo3Version())->getMajorVersion() < 12) {
+        // Register hook to overwrite fragments in page links (RTE and TCA):
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typolinkProcessing']['typolinkModifyParameterForPageLinks'][] = ReplaceFragment::class;
+    }
+})();
