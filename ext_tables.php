8e820dc8d9d937bc0c6e4d5dbc626898f96a0db0
Sebastian Klein <laitnin@gmx.net>
2023-01-28 17:48:00 +0100

    [TASK] Register Context Sensitive Help only for TYPO3 v11

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,16 @@
 <?php
 
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 defined('TYPO3') or die();
 
-ExtensionManagementUtility::addLLrefForTCAdescr(
-    'tt_content',
-    'EXT:content_slug/Resources/Private/Language/locallang_csh_tt_content.xlf'
-);
+(static function () {
+    // TODO: Remove CSH when support for TYPO3 v11 is dropped.
+    if ((new Typo3Version())->getMajorVersion() < 12) {
+        ExtensionManagementUtility::addLLrefForTCAdescr(
+            'tt_content',
+            'EXT:content_slug/Resources/Private/Language/locallang_csh_tt_content.xlf'
+        );
+    }
+})();
