1f0a8d91dc34a46e30fd352cad8550c237c43712
Sebastian Klein <laitnin@gmx.net>
2023-07-29 13:46:28 +0200

    [TASK] Avoid use of TBE_EDITOR and inline JavaScript
    Inline JavaScript for custom eval functions are deprecated
    in TYPO3 12.4.
    
    Switching to JavaScriptModuleInstruction also allows to replace
    TBE_EDITOR.customEvalFunctions in FillFragment.js with the
    new FragmentEvaluation module.
    
    To support both TYPO3 v11 and v12, the deprecated RequireJS
    module loader is used.

--- Resources/Public/JavaScript/FillFragment.js
+++ Resources/Public/JavaScript/FillFragment.js
@@ -1,30 +1,32 @@
 /**
- * Module: TYPO3/CMS/ContentSlug/FillFragment
+ * @exports TYPO3/CMS/ContentSlug/FillFragment
  */
-define([], function () {
-  'use strict';
+define(
+  ['require', 'TYPO3/CMS/ContentSlug/FragmentEvaluation'],
+  function (require, FragmentEvaluation) {
+    'use strict';
 
-  let FillFragment = {};
+    let FillFragment = {};
 
-  /**
-   * When the 'Generate anchor' button is clicked, the anchor/fragment field gets filled with the current content of the header field.
-   * The 'FragmentEvaluation' function is used for an immediate evaluation of the fragment.
-   * 'change' event is needed to successfully save the changes to the database. It also triggers the 'Unsaved changes' modal for this field.
-   */
-  FillFragment.initializeEvents = function () {
-    const fragmentBtn = document.querySelector('.btn-fragment'),
+    /**
+     * When the 'Generate anchor' button is clicked, the anchor/fragment field gets filled with the current content of the header field.
+     * The 'FragmentEvaluation' function is used for an immediate evaluation of the fragment.
+     * 'change' event is needed to successfully save the changes to the database. It also triggers the 'Unsaved changes' modal for this field.
+     */
+    FillFragment.initializeEvents = function () {
+      const fragmentBtn = document.querySelector('.btn-fragment'),
         elemId = fragmentBtn.dataset.uid,
         headerField = document.querySelector('[data-formengine-input-name="data[tt_content][' + elemId + '][header]"]'),
         fragmentField = document.querySelector('[data-formengine-input-name="data[tt_content][' + elemId + '][tx_content_slug_fragment]"]');
 
-    fragmentBtn.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      fragmentField.value = TBE_EDITOR.customEvalFunctions['Sebkln\\ContentSlug\\Evaluation\\FragmentEvaluation'](headerField.value);
-      fragmentField.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
-    });
-  };
+      fragmentBtn.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        fragmentField.value = FragmentEvaluation.evaluateFragment(headerField.value);
+        fragmentField.dispatchEvent(new Event('change', {bubbles: true, cancelable: true}));
+      });
+    };
 
-  FillFragment.initializeEvents();
+    FillFragment.initializeEvents();
 
-  return FillFragment;
+    return FillFragment;
 });
